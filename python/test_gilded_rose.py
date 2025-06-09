198926de351793c252df5e8986019bac0a73b4cc
Amadeo Durgaj <thundahlovesjojo@gmail.com>
2025-06-09 18:47:51 +0200

    Added test: item quality never exceeds 50

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -52,6 +52,11 @@ class GildedRoseTest(unittest.TestCase):
         GildedRose(items).update_quality()
         self.assertEqual(80, items[0].quality)
         self.assertEqual(0, items[0].sell_in)
+
+    def test_quality_never_exceeds_50(self):
+        items = [Item("Aged Brie", 2, 50)]
+        GildedRose(items).update_quality()
+        self.assertEqual(50, items[0].quality)   
     
 if __name__ == '__main__':
     unittest.main()
